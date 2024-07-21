ce5c8e8c9f392b9f9bd0fa02d19f4de196f6c767
Queen Nashami <wanzanashami@gmail.com>
2024-07-21 19:42:39 +0300

    Passes all tests

--- lib/debug.py
+++ lib/debug.py
@@ -3,25 +3,28 @@
 from __init__ import CONN, CURSOR
 from department import Department
 from employee import Employee
-import ipdb
 
+import random
+from faker import Faker
+
+fake = Faker()
 
 def reset_database():
-    Employee.drop_table()
     Department.drop_table()
     Department.create_table()
+    Employee.drop_table()
     Employee.create_table()
 
-    # Create seed data
     payroll = Department.create("Payroll", "Building A, 5th Floor")
-    human_resources = Department.create(
-        "Human Resources", "Building C, East Wing")
-    Employee.create("Amir", "Accountant", payroll.id)
-    Employee.create("Bola", "Manager", payroll.id)
-    Employee.create("Charlie", "Manager", human_resources.id)
-    Employee.create("Dani", "Benefits Coordinator", human_resources.id)
-    Employee.create("Hao", "New Hires Coordinator", human_resources.id)
+    hr = Department.create("Human Resources", "Building C, East Wing")
 
+    job_titles = ['Manager', 'Accountant', 'Clerk', 'Assistant', 'Coordinator']
+
+    for _ in range(5):
+        name = fake.first_name()
+        job_title = random.choice(job_titles)
+        department_id = random.choice([payroll.id, hr.id])
+        Employee.create(name, job_title, department_id)
 
 reset_database()
-ipdb.set_trace()
+import ipdb; ipdb.set_trace()
